09af5947f48a2184b897e29f79a1dcd98492a377
Amitava Datta <dattaamitava2004@gmail.com>
2025-09-05 17:10:09 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: 'civicpulse-pwa',
-  appId: '1:764129977591:web:bdc6c167602968acce5ff2',
-  storageBucket: 'civicpulse-pwa.firebasestorage.app',
-  apiKey: 'AIzaSyDbdimmanMdIRhzxzGDO0nZ5-Bn4-Dq2Vg',
-  authDomain: 'civicpulse-pwa.firebaseapp.com',
-  messagingSenderId: '764129977591',
+  apiKey: "AIzaSyDbdimmanMdIRhzxzGDO0nZ5-Bn4-Dq2Vg",
+  authDomain: "civicpulse-pwa.firebaseapp.com",
+  projectId: "civicpulse-pwa",
+  storageBucket: "civicpulse-pwa.firebasestorage.app",
+  messagingSenderId: "764129977591",
+  appId: "1:764129977591:web:bdc6c167602968acce5ff2"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
